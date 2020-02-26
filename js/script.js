077e1711340ff6966ea6b249fa2aaa1ee1d0a095
lorenzo lulli <lullilore@gmail.com>
2020-02-26 17:54:12 +0100

    aggiunta controlli

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ console.log(numeriRandom);
 
 
 
-for (var i = 0; i < 83; i++) {
-var tentativo = parseInt(prompt('scrivi un numero'));
+for (var i = 0; i < 10; i++) {
+var tentativo = parseInt(prompt('scrivi un numero da 1 a 100'));
 
 console.log(numeroTenativi);
 console.log(tentativo);
@@ -24,12 +24,28 @@ if (!numeriRandom.includes(tentativo)) {
 
 
     if (!numeroTenativi.includes(tentativo)) {
-        numeroTenativi.push(tentativo);
-        console.log('scegli altro numero');
+
+
+        if (!isNaN(tentativo)) {
+
+            if (tentativo < 101 && tentativo >= 1) {
+
+
+                console.log('scegli altro numero');
+                numeroTenativi.push(tentativo);
+            }
+            else {
+
+                console.log('scegli un numero compreso tra 1 e 100');
+            }
+        }
+        else {
+            console.log('hai scritto una parola non un numero');
+        }
 
     }
     else {
-        
+
         console.log('numero ripetuto');
 
     }
